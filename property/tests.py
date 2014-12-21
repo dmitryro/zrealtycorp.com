b9ab11124ec5ce3d2a96677e9aa4b47073acdc5f
Dmitry Roitman <dmitryro@gmail.com>
2014-12-21 15:46:25 -0600

    property unit tests

--- property/tests.py
+++ property/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
-
+from models import Status, Type, Category, Room
 
 # Create your tests here.
 def setUp(self):
@@ -15,4 +15,63 @@ class PropertyTest(TestCase):
     def test_serialization(self):
         pass
 
+
+
+class StatusTestCase(TestCase):
+    
+    def setUp(self):
+        st = Status.objects.create(status='mock')
+
+    def tearDown(self):
+        pass
+ 
+    def test_status_creation(self):
+        status = Status.objects.get(status='mock')
+        self.assertEqual(status.status,'mock')
+        status.delete() # Hust clean up
+
+class StatusTestCase(TestCase):
+
+    def setUp(self):
+        st = Status.objects.create(status='mock')
+
+    def tearDown(self):
+        pass
+
+    def test_status_creation(self):
+        status = Status.objects.get(status='mock')
+        self.assertEqual(status.status,'mock')
+        status.delete() # Hust clean up
+
+class TypeTestCase(TestCase):
+
+    def setUp(self):
+        st = Type.objects.create(type='mock')
+
+    def tearDown(self):
+        pass
+
+    def test_type_creation(self):
+        type = Type.objects.get(type='mock')
+        self.assertEqual(type.type,'mock')
+        type.delete() # Hust clean up
+
+
+class CategoryTestCase(TestCase):
+
+    def setUp(self):
+        st = Category.objects.create(category='mock')
+
+    def tearDown(self):
+        pass
+
+    def test_category_creation(self):
+        category = Category.objects.get(category='mock')
+        self.assertEqual(category.category,'mock')
+        category.delete() # Hust clean up
+
+
+
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
 
