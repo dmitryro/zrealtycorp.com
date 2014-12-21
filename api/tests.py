d9079b2c34272b29a7fdc4509e7cf5880d7032d2
Dmitry Roitman <dmitryro@gmail.com>
2014-12-21 11:32:56 -0600

    user profile test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from api.models import UserProfile
+
+class UserProfileTestCase(TestCase):
+    def setUp(self):
+        UserProfile.objects.create(user_id=1041,activation_key='somewirdhash')          UserProfile.objects.create(user_id=1042,activation_key='somewirdhash')        
+    def test_user_profile_has_id(self):
+         user = UserProfile.objects.get(user_id=1041)
+         another = UserProfile.objects.get(user_id=1041)
+         self.assertEqual(user.user_id,1041)
+         self.assertEqual(another.user_id,1042)  
+
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
 
 # Create your tests here.
