731ac1f603792d7151e0704f593d47a4e3979c44
Dmitry Roitman <dmitryro@gmail.com>
2014-12-24 16:25:36 -0600

    tastypie oauth test

--- api/api.py
+++ api/api.py
@@ -1,9 +1,11 @@
 from rest_framework import generics, permissions
 from tastypie.resources import ModelResource
 from models import UserProfile
+from tastypie.cache import SimpleCache
 from tastypie import fields
 
 class ProfileResource(ModelResource):
     class Meta:
         queryset = UserProfile.objects.all()
         resource_name = 'profile'
+        cache = SimpleCache(timeout=10)
