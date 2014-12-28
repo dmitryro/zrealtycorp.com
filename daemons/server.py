2fc34a364203b165dea3b4c0e0bbe254dde595cc
Dmitry Roitman <dmitryro@gmail.com>
2014-12-27 18:53:38 -0600

    simple daemon

--- daemons/server.py
+++ daemons/server.py
@@ -1,12 +1,14 @@
 import logging
 import logging.handlers
-import clienttypes
 import daemon
 import os
 import urllib2
+from celery import Celery
 
 with daemon.DaemonContext():
-
-
     if __name__ == "__main__":
-        pass
+        app = Celery('tasks', broker='redis://localhost:6379/0')
+
+        @app.task
+        def add(x, y):
+            return x+y
