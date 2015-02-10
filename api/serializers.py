b912888fc4e929bc5342d0b117b2af4bd74f60ec
Dmitry Roitman <dmitryro@gmail.com>
2015-02-09 22:12:45 -0500

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -6,6 +6,8 @@ import json
 
 class UserProfileSerializer(serializers.ModelSerializer):
     id = serializers.Field()
+    username = serializers.CharField(max_length=140,read_only=True)
+    activation_key = serializers.CharField(max_length=140,read_only=True)
 
     class Meta:
         model = UserProfile
@@ -13,6 +15,8 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         if instance:
+            instance.username = validated_data.get('username', instance.username)
+            instance.activation_key = validated_data.get('activation_key', instance.activation_key)
             instance.id = validated_data.get('id', instance.id)
             instance.save()
             return instance
@@ -25,6 +29,8 @@ class UserProfileSerializer(serializers.ModelSerializer):
         if instance:
            ret = super(UserProfileSerializer, self).to_representation(self)
            ret['id']= instance.id
+           ret['username']= instance.username
+           ret['activation_key']= instance.activation_key
            return ret
 
 
